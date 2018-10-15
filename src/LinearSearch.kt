2ce8eb17de1bfefd93f984bdf8f8b55853d98deb
Julian Henao <julianhg28@gmail.com>
2018-10-14 22:41:07 -0300

    Add LinearSearch.kt

---
+++ src/LinearSearch.kt
@@ -0,0 +1,26 @@
+
+private fun linearSearch(array: IntArray, target: Int): Int{
+    val NOT_FOUND = -1
+    var foundAt: Int = NOT_FOUND
+    if (array.isNotEmpty()){
+        var index: Int = 0
+        while (foundAt == NOT_FOUND &&
+                index <= array.lastIndex){
+            println("Ciclo ${index + 1}")
+            if (array[index] == target){ foundAt = index}
+            else if (array[index] > target){ break }
+            else { index++ }
+        }
+    }
+    return foundAt
+}
+
+fun main(args: Array<String>) {
+
+    val TARGET_VALUE = 15
+    val myArray = intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+    val isFoundAt = linearSearch(myArray, TARGET_VALUE)
+
+    if (isFoundAt >= 0){ println(" $TARGET_VALUE is found at $isFoundAt index.")}
+    else{ println("Target not found!")}
+}
